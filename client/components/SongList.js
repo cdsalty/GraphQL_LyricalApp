c3b164743d9b16ecf8bdcb445ebb621f842217a6
cdsalty <soltiscd@gmail.com>
2020-12-15 17:29:42 -0500

    added unique, still need to update/refactor some code

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,26 +1,38 @@
-// import React, { Component } from "react";
 import gql from "graphql-tag"; // helper to write component queries
 import { graphql } from "react-apollo"; // react-apollo library is similar to the glue layer of the data source to help bond a component with an actual query
 
-// Functional Component route...
-
 /*
 SongList.js (fetch a list of songs and rendering on the screen)
-- in the lesson, it was made with class based components. Will follow lesson but then change into hooks
 
 */
+// const SongList = ({ data }) => {
+//   const renderSongs = data => {
+//     return data.songs.map(song => {
+//       return <li key={song.id} className = "collection-item">{song.title}</li>;
+//     });
+//   };
+//   // "load" is handled externally, doesn't need to be state
+//   if (data.loading) {
+//     return <div>Loading</div>;
+//   }
+//   return <ul>{renderSongs(data)}</ul>;
+// };
 
 const SongList = ({ data }) => {
   const renderSongs = data => {
     return data.songs.map(song => {
-      return <li>{song.title}</li>;
+      return (
+        <li key={song.id} className="collection-item">
+          {song.title}
+        </li>
+      );
     });
   };
-  // handled externally, doesn't need to be state
+  // "load" is handled externally, doesn't need to be state
   if (data.loading) {
     return <div>Loading</div>;
   }
-  return <div>{renderSongs(data)}</div>;
+  return <li>{renderSongs(data)}</li>;
 };
 
 const query = gql`
